497b37d35e2a5c5d2bf5b6ea4664be30ff99678a
Edzass <93673275+Edzass@users.noreply.github.com>
2026-09-14 09:25:56 +0300

    korekcija 14.09

--- Hello.cs
+++ Hello.cs
@@ -4,9 +4,16 @@ using System;
 public partial class Hello : Node
 {
 	// Called when the node enters the scene tree for the first time.
+	private int counter = 0;
+	private string playerName = "Edgars";
+	private Vector2 spawnPosition = new Vector2(640, 360);
 	public override void _Ready()
 	{
-		GD.Print("Sveiks no C#!");
+
+		GD.Print("Sveiks " + playerName + " !");
+		GD.Print(counter);
+		GD.Print(spawnPosition);
+	
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
--- README.md
+++ README.md
@@ -16,3 +16,14 @@ Node 2D ir objekta iesatījums pozīcijai, rotācijai un mēroga attēlošanai 2
 Scena ir kā mezglu apkopojums, mezgli padara aritektūrtu vieglāk pārredzamu un uztveramu.
 
 dotnet versija --10.0.401
+
+Hello.cs un Hello nosaukumiem ir jāsakrīt, lai 
+Izdzēšot semikolu pēc GD.Print(...) parādās kļūda "error CS1002: ; expected".
+
+| Python   | C#		 |
+| -------- | ------- |
+| January  | $250    |
+| February | $80     |
+| March    | $420    |
+
+ghjkl
